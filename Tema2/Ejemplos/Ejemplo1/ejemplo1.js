9de38ea6beafd68bf4eae8e7792898eb9d271bc5
Comparitiko <gabigcy@gmail.com>
2024-09-25 17:19:42 +0200

    First commit

---
+++ Tema2/Ejemplos/Ejemplo1/ejemplo1.js
@@ -0,0 +1,20 @@
+document.getElementById("main-title").innerText =
+  "Hola Mundo con document.getElementById"; // Escribe en el elemento con id main-title
+document.write("<h1>Hola Mundo con document.write</h1>"); // Escribe en el HTML
+console.log("Hola Mundo");
+
+// const nombre = prompt("Introduce tu nombre");
+// document.write(`<h1>Hola ${nombre}</h1>`); // Escribe en el HTML
+
+// const number = parseInt(prompt("Introduce un número entero"));
+// document.write(
+//   `<h2>El numero introducido sumandole 100 es ${number + 100}</h2> `
+// );
+
+// console.log(number);
+
+const res = confirm("¿Te gusta el futbol?");
+
+const message = res ? "Te gusta el futbol" : "No te gusta el futbol";
+
+alert(message);
