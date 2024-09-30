bd04e350f7bd54a04a0c6cb93089438d3d26d6f3
Comparitiko <gabigcy@gmail.com>
2024-09-30 17:57:45 +0200

    Hecho ejercicios relacion 2

--- Tema2/Practicas/practica1/practica1.js
+++ Tema2/Practicas/practica1/practica1.js
@@ -92,46 +92,171 @@
 
 // document.querySelector("#hidden-table").classList.remove("oculto");
 
-const tiempos = {
-  SOL: "SOL",
-  NUBLADO: "NUBLADO",
-  LLUVIA: "LLUVIA",
+// // Ejercicio 33
+
+// const tiempos = {
+//   SOL: "SOL",
+//   NUBLADO: "NUBLADO",
+//   LLUVIA: "LLUVIA",
+// };
+
+// const predicciones = {
+//   SOLSOL: "SOL",
+//   NUBLADONUBLADO: "NUBLADO",
+//   LLUVIALLUVIA: "LLUVIA",
+//   SOLNUBLADO: "NUBLADO",
+//   NUBLADOSOL: "SOL",
+//   SOLLLUVIA: "NUBLADO",
+//   LLUVIASOL: "NUBLADO",
+//   NUBLADOLLUVIA: "LLUVIA",
+//   LLUVIANUBLADO: "NUBLADO",
+// };
+
+// const tiempo = (ayer, hoy) => {
+//   if ((!ayer, !hoy))
+//     throw new Error(
+//       "Introduce el tiempo que hizo ayer y el que esta haciendo hoy"
+//     );
+
+//   const tiempoAyer = tiempos[ayer.toUpperCase()];
+//   const tiempoHoy = tiempos[hoy.toUpperCase()];
+
+//   if (!tiempoAyer || !tiempoHoy)
+//     throw new Error("Introduce los valores correctamente");
+
+//   const tiempoAyerHoy = `${tiempoAyer}${tiempoHoy}`;
+
+//   const prediccion = predicciones[tiempoAyerHoy];
+
+//   alert(`El tiempo para mañana sera de ${prediccion}`);
+// };
+
+// const ayer = prompt("Ingrese el tiempo de ayer", "nublado");
+// const hoy = prompt("Ingrese el tiempo de hoy", "nublado");
+
+// console.log(tiempos[ayer]);
+
+// tiempo(ayer, hoy);
+
+// // Ejercicio 39
+
+// const reqNum = () => {
+//   let num;
+//   do {
+//     num = parseInt(prompt("Ingrese un numero"));
+//   } while (isNaN(num));
+//   return num;
+// };
+
+// const isPerfectNumber = (numero) => {
+//   let divisores = [];
+
+//   for (let i = 1; i <= numero / 2; i++) {
+//     if (numero % i === 0) divisores.push(i);
+//   }
+
+//   const suma = divisores.reduce((prev, curr) => prev + curr);
+
+//   return suma === numero;
+// };
+
+// const num = reqNum();
+
+// const isPerfect = isPerfectNumber(num);
+
+// alert(
+//   isPerfect ? `El numero ${num} es perfecto` : `El numero ${num} no es perfecto`
+// );
+
+// // Ejercicio 3 relacion 2
+// for (let i = 1; i <= 300; i++) {
+//   const num = document.createElement("p");
+
+//   num.innerText = i;
+
+//   if (i % 2 === 0 && i % 3 === 0) {
+//     num.classList.add("multiplo-2-3");
+//   } else if (i % 2 === 0) {
+//     num.classList.add("multiplo-2");
+//   } else if (i % 3 === 0) {
+//     num.classList.add("multiplo-3");
+//   }
+
+//   document.body.appendChild(num);
+// }
+
+// // Ejercicio 1 relacion 2
+
+// const generarTabla = (filas, columnas) => {
+//   const table = document.createElement("table");
+
+//   for (let i = 1; i <= filas; i++) {
+//     const tr = document.createElement("tr");
+//     for (let j = 1; j <= columnas; j++) {
+//       const td = document.createElement("td");
+
+//       td.innerText = `${i}, ${j}`;
+
+//       if (j % 2 === 0) td.classList.add("primo");
+
+//       tr.appendChild(td);
+//     }
+//     table.appendChild(tr);
+//   }
+
+//   document.body.appendChild(table);
+// };
+
+// generarTabla(10, 20);
+
+// Ejercicio 2 relacion 2
+const numInput = document.querySelector("#num-dni");
+const letraInput = document.querySelector("#letra-dni");
+
+const letras = {
+  0: "T",
+  1: "R",
+  2: "W",
+  3: "A",
+  4: "G",
+  5: "M",
+  6: "Y",
+  7: "F",
+  8: "P",
+  9: "D",
+  10: "X",
+  11: "B",
+  12: "N",
+  13: "J",
+  14: "Z",
+  15: "S",
+  16: "Q",
+  17: "V",
+  18: "H",
+  19: "L",
+  20: "C",
+  21: "K",
+  22: "E",
 };
 
-const predicciones = {
-  SOLSOL: "SOL",
-  NUBLADONUBLADO: "NUBLADO",
-  LLUVIALLUVIA: "LLUVIA",
-  SOLNUBLADO: "NUBLADO",
-  NUBLADOSOL: "SOL",
-  SOLLLUVIA: "NUBLADO",
-  LLUVIASOL: "NUBLADO",
-  NUBLADOLLUVIA: "LLUVIA",
-  LLUVIANUBLADO: "NUBLADO",
-};
+numInput.addEventListener("keyup", (e) => {
+  if (isNaN(parseInt(e.key))) {
+    const numArr = numInput.value.split("");
+    numArr[numArr.length - 1] = "";
+    numInput.value = numArr.join("");
+  }
 
-const tiempo = (ayer, hoy) => {
-  if ((!ayer, !hoy))
-    throw new Error(
-      "Introduce el tiempo que hizo ayer y el que esta haciendo hoy"
-    );
+  const num = numInput.value;
 
-  const tiempoAyer = tiempos[ayer.toUpperCase()];
-  const tiempoHoy = tiempos[hoy.toUpperCase()];
+  if (num.length < 8) return;
 
-  if (!tiempoAyer || !tiempoHoy)
-    throw new Error("Introduce los valores correctamente");
+  const numArr = num.split("");
 
-  const tiempoAyerHoy = `${tiempoAyer}${tiempoHoy}`;
+  const sumaNums = numArr.reduce((prev, curr) => {
+    return prev + parseInt(curr);
+  });
 
-  const prediccion = predicciones[tiempoAyerHoy];
+  const letra = letras[sumaNums % 23];
 
-  alert(`El tiempo para mañana sera de ${prediccion}`);
-};
-
-const ayer = prompt("Ingrese el tiempo de ayer", "nublado");
-const hoy = prompt("Ingrese el tiempo de hoy", "nublado");
-
-console.log(tiempos[ayer]);
-
-tiempo(ayer, hoy);
+  letraInput.value = letra;
+});
